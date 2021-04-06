4279c67534cb387546b4b9504c58dbecffac1618
Joshhortt <joshhortt@yahoo.com>
2021-04-06 10:20:21 +0100

    Add 01 - SQL Server project.Add DataAccessLibrary project + SQLContactsDB project. Add Tables. Add appsettings.jason with connectionStrings to connect Local DB. Publish DB.

---
+++ 01-SqlServer/RelationalDBSolution/DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+	// Creating data 40. -- > 42.
+	public class SqlDataAccess
+	{
+		public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+		{
+			using (IDbConnection connection = new SqlConnection(connectionString))
+			{ 
+			List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+			return rows;
+			}
+		}
+
+		// Saving data 43. -- > 44.
+		public void SaveData<T, U>(string sqlStatement, T parameters, string connectionString)
+		{
+			using (IDbConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Execute(sqlStatement, parameters);
+			}
+		}
+	}
+}
---
+++ 01-SqlServer/RelationalDBSolution/SQLServerUI/Program.cs
@@ -0,0 +1,30 @@
+﻿// 01 - C # Data Access - SQL Server
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SQLServerUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Console.WriteLine(GetConnectionString()); Teste Line 
+			Console.ReadLine();
+		}
+
+		private static string GetConnectionString(string connectionStringName = "Default")
+		{
+			string output = "";
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			var config = builder.Build();
+			output = config.GetConnectionString(connectionStringName);
+
+			return output;
+		}
+	}
+}
