4fb1200ea394df4b5bbf89f61ed7f9aee960cfc3
Joshhortt <joshhortt@yahoo.com>
2021-04-09 11:53:33 +0100

    FC of project 02 - Sqlite - data Access. Create similar new project like SQLServerUI.

---
+++ 02-Sqlite/RelationalDBSolution/SqliteUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLibrary;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SqliteUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            SqliteCrud sql = new SqliteCrud(GetConnectionString());
+
+            // ReadAllContacts(sql);
+
+            // ReadContact(sql, 3);
+
+            // CreateNewContact(sql);
+
+            // UpdateContact(sql);
+
+            // RemovePhoneNumberFromContact(sql, 1, 1);
+
+            Console.WriteLine("Processing Sqlite");
+
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
+}
