df1b329bc04997262c283073f05b4662082cfab2
Joshhortt <joshhortt@yahoo.com>
2021-04-20 18:02:16 +0100

    Implement GetAllContacts method and call add method

--- 04-MongoDB/NoSqlDbSolution/MongoDbUI/Program.cs
+++ 04-MongoDB/NoSqlDbSolution/MongoDbUI/Program.cs
@@ -20,23 +20,41 @@ namespace MongoDbUI
 
             ContactModel user = new ContactModel
             {
-                FirstName = "Josh",
-                LastName = "Hortt"
+                //CreateContact - 1
+                //FirstName = "Josh",
+                //LastName = "Hortt"
+
+                //CreateContact - 2
+                FirstName = "Ana",
+                LastName = "Rebelo"
             };
-            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "joshhortt@yahoo.com" });
-            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "jose@gmx.com" });
-            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "911815877" });
-            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "927916476" });
+            //user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "joshhortt@yahoo.com" }); -1
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "ana@gmx.com" });  // 2
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "jose@gmx.com" }); // 1
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "911815877" });  // 1
+            //user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "927916476" });  // 1
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "906122005" });  // 2
 
-            CreateContact(user);
-
+            // CreateContact(user);
+               GetAllContacts();
 
             Console.WriteLine("Done Processing MongoDB!");
             Console.ReadLine();
         }
 
+        // Get All Contacts
+        private static void GetAllContacts()
+        {
+            var contacts = db.LoadRecords<ContactModel>(tableName);
 
-		private static void CreateContact(ContactModel contact)
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{ contact.Id}: { contact.FirstName } { contact.LastName }");
+            }
+        }
+
+        // Create Contacts
+        private static void CreateContact(ContactModel contact)
 		{
 			db.UpsertRecord(tableName, contact.Id, contact);
 		}
