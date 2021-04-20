4c1dfd4d2f5eb39ebda0197a8e540399b46b3cdc
Joshhortt <joshhortt@yahoo.com>
2021-04-20 09:23:23 +0100

    Add Program files. Add GetConnectionString method to program.cs, add ConnectionStrings to appsettings.json.

---
+++ 04-MongoDB/NoSqlDbSolution/MongoDbUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace MongoDbUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Done Processing MongoDB!");
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
+}
