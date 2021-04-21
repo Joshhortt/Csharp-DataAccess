1cfb1bf843b94bc779a13e74dc5a2f27a082637d
Joshhortt <joshhortt@yahoo.com>
2021-04-21 16:11:04 +0100

    Add Implementation of method 'ReadAllRecords'.

--- 06-TextFiles/TextFileSolution/DataAccessLibrary/TextFileDataAccess.cs
+++ 06-TextFiles/TextFileSolution/DataAccessLibrary/TextFileDataAccess.cs
@@ -10,7 +10,40 @@ namespace DataAccessLibrary
 {
     public class TextFileDataAccess
     {
-        public void WriteAllRecords(List<ContactModel> contacts, string textFile)
+        // READ ALL RECORDS
+        public static List<ContactModel> ReadAllRecords(string textFile)
+        {
+            if (File.Exists(textFile) == false)
+            {
+                return new List<ContactModel>();
+            }
+
+            var lines = File.ReadAllLines(textFile);
+            List<ContactModel> output = new List<ContactModel>();
+
+            foreach (var line in lines)
+            {
+                ContactModel c = new ContactModel();
+                var values = line.Split(',');
+
+                if (values.Length < 4)
+                {
+                    throw new Exception($"This a Invalid row of data: { line }");
+                }
+
+                c.FirstName = values[0];
+                c.LastName = values[1];
+                c.EmailAddresses = values[2].Split(';').ToList();
+                c.PhoneNumbers = values[3].Split(';').ToList();
+
+                output.Add(c);
+            }
+
+            return output;
+        }
+
+        // WRITE ALL RECORDS
+        public static void WriteAllRecords(List<ContactModel> contacts, string textFile)
         {
             List<string> lines = new List<string>();
 
