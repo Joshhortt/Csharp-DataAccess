281f15861f95dbadb3951a4c0329bdeec57cd532
Joshhortt <joshhortt@yahoo.com>
2021-04-21 16:02:24 +0100

    Add DataAccessLibrary with class 'TextFileDataAccess.' + implement method 'WriteAllRecords'. Create a ContactModel with properties.

---
+++ 06-TextFiles/TextFileSolution/DataAccessLibrary/TextFileDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class TextFileDataAccess
+    {
+        public void WriteAllRecords(List<ContactModel> contacts, string textFile)
+        {
+            List<string> lines = new List<string>();
+
+			foreach (var c in contacts)
+			{
+				lines.Add($"{ c.FirstName },{ c.LastName },{ String.Join(';', c.EmailAddresses) },{ String.Join(';', c.PhoneNumbers) }");
+			}
+
+			File.WriteAllLines(textFile, lines);
+		}
+    }
+}
