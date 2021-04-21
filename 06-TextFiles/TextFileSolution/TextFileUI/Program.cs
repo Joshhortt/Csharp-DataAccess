72ee36af0f05855948923b531e3978d94d3824fa
Joshhortt <joshhortt@yahoo.com>
2021-04-21 10:44:30 +0100

    Add Program Files of DataAccess - working with 'TextFiles'. Add Initial Configuration & TextFile Path

---
+++ 06-TextFiles/TextFileSolution/TextFileUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace TextFileUI
+{
+	class Program
+	{
+        private static IConfiguration _config;
+		private static string textFile;
+
+		static void Main(string[] args)
+		{
+            InitializeConfiguration();
+
+				textFile = _config.GetValue<string>("TextFile");
+
+            Console.ReadLine();
+		}
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+        }
+    }
+}
