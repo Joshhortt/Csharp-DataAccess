81691677b50569fbe95bb65a57293560ffa592fc
Joshhortt <joshhortt@yahoo.com>
2021-04-24 14:52:10 +0100

    Turn on Logging

--- 09-EntityFramework/EntityFrameworkIntro/Program.cs
+++ 09-EntityFramework/EntityFrameworkIntro/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -99,7 +100,7 @@ class CookbookContextFactory : IDesignTimeDbContextFactory<CookbookContext>
 		optionsBuilder
 			// Uncomment the following line if you want to print generated
 			// SQL statements on the console.
-			// .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+			.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
 			.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
 
 		return new CookbookContext(optionsBuilder.Options);
